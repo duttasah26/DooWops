3bcf76ede24ab9487c82f7d53f94d24ea2a56390
duttasah <duttasah@sheridancollege.ca>
2025-03-26 00:33:41 -0400

    Setup MongoDb connection

--- backend/server.js
+++ backend/server.js
@@ -34,3 +34,51 @@ io.on("connection", (socket) => {
         console.log(`User disconnected: ${socket.id}`);
     });
 });
+
+//connecting to MongoDB Atlas using Connection String from .env
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("MongoDB Connected"))
+    .catch(err => console.log(err));
+
+
+//GET /api/playlist/:playlistId
+//this is custom API route for the FRONTEND; 
+
+app.get("/api/playlist/:playlistId",async(req,res)=> {
+    const playlistId = req.params.playlistId;
+
+    try {
+        const auth = Buffer.from(`${process.env.SPOTIFY_CLIENT_ID}:${process.env.SPOTIFY_CLIENT_SECRET}`).toString("base64");
+
+        const tokenResponse= await axios.post //fetching token
+        (
+            "https://accounts.spotify.com/api/token",
+            "grant_type=client_credentials",
+            {
+                headers: {
+                    Authorization: `Basic ${auth}`,
+                    "Content-Type": "application/x-www-form-urlencoded",
+                },
+            }
+        );
+
+        //getting token from spotify response
+        const accessToken = tokenResponse.data.access_token;
+
+        //fetching playlist tracks
+        const response = await axios.get(
+            `https://api.spotify.com/v1/playlists/${playlistId}/tracks`,
+            {
+                headers: { Authorization: `Bearer ${accessToken}` }, // Use token in the request header
+            }
+        );
+
+        res.json(response.data);
+    }
+    catch(error)
+    {
+        console.error(error);
+        res.status(500).json({error:"Error fetching playlist"});
+
+    }
+});
